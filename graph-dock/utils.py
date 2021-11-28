3af5e38f01253ca4f401ada735a35ee57368131a
Ryien Hosseini <ryienh@umich.edu>
2021-11-28 16:02:52 -0600

    Adds utils for training checkpoints

--- graph-dock/utils.py
+++ graph-dock/utils.py
@@ -4,6 +4,8 @@ Collection of utility functions for training and evaluation scripts.
 For questions or comments, contact rhosseini@anl.gov
 """
 import os
+import torch
+import itertools
 
 
 def config(attr, fname=os.path.join("./", "graph-dock", "config.json")):
@@ -62,3 +64,101 @@ class suppress_stdout_stderr(object):
         # Close all file descriptors
         for fd in self.null_fds + self.save_fds:
             os.close(fd)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def save_checkpoint(model, epoch, checkpoint_dir, stats):
+    state = {
+        "epoch": epoch,
+        "state_dict": model.state_dict(),
+        "stats": stats,
+    }
+
+    filename = os.path.join(checkpoint_dir, "epoch={}.checkpoint.pth.tar".format(epoch))
+    torch.save(state, filename)
+
+
+def restore_checkpoint(model, checkpoint_dir, cuda=True, force=False, pretrain=False):
+    """
+    If a checkpoint exists, restores the PyTorch model from the checkpoint.
+    Returns the model and the current epoch.
+    """
+    files = [
+        fn
+        for fn in os.listdir(checkpoint_dir)
+        if fn.startswith("epoch=") and fn.endswith(".checkpoint.pth.tar")
+    ]
+
+    if not files:
+        print("No saved model parameters found")
+        if force:
+            raise Exception("Checkpoint not found")
+        else:
+            return model, 0, []
+
+    # Find latest epoch
+    for i in itertools.count(1):
+        if "epoch={}.checkpoint.pth.tar".format(i) in files:
+            epoch = i
+        else:
+            break
+
+    if not force:
+        print(
+            "Which epoch to load from? Choose in range [0, {}].".format(epoch),
+            "Enter 0 to train from scratch.",
+        )
+        print(">> ", end="")
+        inp_epoch = int(input())
+        if inp_epoch not in range(epoch + 1):
+            raise Exception("Invalid epoch number")
+        if inp_epoch == 0:
+            print("Checkpoint not loaded")
+            clear_checkpoint(checkpoint_dir)
+            return model, 0, []
+    else:
+        print("Which epoch to load from? Choose in range [1, {}].".format(epoch))
+        inp_epoch = int(input())
+        if inp_epoch not in range(1, epoch + 1):
+            raise Exception("Invalid epoch number")
+
+    filename = os.path.join(
+        checkpoint_dir, "epoch={}.checkpoint.pth.tar".format(inp_epoch)
+    )
+
+    print("Loading from checkpoint {}?".format(filename))
+
+    if cuda:
+        checkpoint = torch.load(filename)
+    else:
+        # Load GPU model on CPU
+        checkpoint = torch.load(filename, map_location=lambda storage, loc: storage)
+
+    try:
+        start_epoch = checkpoint["epoch"]
+        stats = checkpoint["stats"]
+        if pretrain:
+            model.load_state_dict(checkpoint["state_dict"], strict=False)
+        else:
+            model.load_state_dict(checkpoint["state_dict"])
+        print(
+            "=> Successfully restored checkpoint (trained for {} epochs)".format(
+                checkpoint["epoch"]
+            )
+        )
+    except:
+        print("=> Checkpoint not successfully restored")
+        raise
+
+    return model, inp_epoch, stats
+
+
+def clear_checkpoint(checkpoint_dir):
+    fnames = [f for f in os.listdir(checkpoint_dir) if f.endswith(".pth.tar")]
+    for f in fnames:
+        os.remove(os.path.join(checkpoint_dir, f))
+
+    print("Checkpoint successfully removed")
